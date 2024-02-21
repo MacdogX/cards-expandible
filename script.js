f3a7290a4c7a2d53ef0daf8c01aaf3eb153eaa90
Miguel Angel <soymmiguelr@gmail.com>
2024-02-20 19:58:19 -0500

    documentacion

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel'); /* Selecciona todos los elementos con la clase 'panel' y los almacena en la variable panels */
 
-panels.forEach(panel=>{
+panels.forEach(panel => { /* Para cada panel seleccionado */
+    panel.addEventListener('click', () => { /* Agrega un evento 'click' */
+        removeActiveClasses(); /* Llama a la función removeActiveClasses() para eliminar todas las clases 'active' */
+        panel.classList.add('active'); /* Agrega la clase 'active' al panel clicado */
+    });
+});
 
-    panel.addEventListener('click',()=>{
-        removeActiveClasses()
-        panel.classList.add('active')
-
-    })
-})
-
-function removeActiveClasses(){
-    panels.forEach(panel=>{
-        panel.classList.remove('active')
-    })
+function removeActiveClasses() { /* Función para eliminar la clase 'active' de todos los paneles */
+    panels.forEach(panel => { /* Para cada panel */
+        panel.classList.remove('active'); /* Elimina la clase 'active' */
+    });
 }
